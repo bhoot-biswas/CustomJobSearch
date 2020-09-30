303178abba5c264ca2954261454a1b6e36f06a9c
Mithun Biswas <bhoot.biswas@gmail.com>
2020-09-30 23:16:17 +0600

    Add initial files

---
+++ custom-job-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Custom Job Search
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     custom-job-search
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Custom_Job_Search
+ */
+
+// Your code starts here.
