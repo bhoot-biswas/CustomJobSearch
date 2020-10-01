58064519365468329c890c5abc5a2e62dd1b7c0b
Mithun Biswas <bhoot.biswas@gmail.com>
2020-10-01 13:41:17 +0600

    Update render_custom_job_search()

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -33,7 +33,35 @@ class Shortcode {
 	 * @return [type]       [description]
 	 */
 	public function render_custom_job_search( $atts = array(), $content = null ) {
-		return 'Hello World!';
+		$permalink = job_manager_get_permalink( 'jobs' );
+		if ( ! $permalink ) {
+			return;
+		}
+		\ob_start();
+		?>
+		<form method="GET" action="<?php echo esc_url( $permalink ); ?>">
+			<p>
+				<label for="keywords"><?php _e( 'Keywords', 'custom-job-search' ); ?></label>
+				<input type="text" id="search_keywords" name="search_keywords" />
+			</p>
+			<p>
+				<label for="keywords"><?php _e( 'Location', 'custom-job-search' ); ?></label>
+				<input type="text" id="search_location" name="search_location" />
+			</p>
+			<p>
+				<label for="search_category"><?php _e( 'Category', 'custom-job-search' ); ?></label>
+				<select id="search_category" name="search_category">
+				<?php foreach ( get_job_listing_categories() as $cat ) : ?>
+					<option value="<?php echo esc_attr( $cat->term_id ); ?>"><?php echo esc_html( $cat->name ); ?></option>
+				<?php endforeach; ?>
+				</select>
+			</p>
+			<p>
+				<input type="submit" value="<?php _e( 'Search', 'custom-job-search' ); ?>" />
+			</p>
+		</form>
+		<?php
+		return \ob_get_clean();
 	}
 }
 
